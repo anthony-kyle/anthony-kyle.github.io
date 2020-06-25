37df5b444209a490ef8107fd565ba2d6a3bfe89d
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-06-25 17:15:26 +1200

    Fix header links

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+//*************************************** */
+// Functions.js                           */
+// Author: Anthony McGrath                */
+//*************************************** */
+
+name = "Anthony";   // String
+age = 32;           // Integer
+
+
+console.log(age + age);
+console.log("32" + "32");
