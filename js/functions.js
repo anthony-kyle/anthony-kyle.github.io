b3e66f4b2addad61c6129229d5fb4f12d752dea9
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-07-07 10:13:33 +1200

    Remove Dummy functions

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,3 @@
 // Functions.js                           */
 // Author: Anthony McGrath                */
 //*************************************** */
-
-name = "Anthony";   // String
-age = 32;           // Integer
-
-
-console.log(age + age);
-console.log("32" + "32");
