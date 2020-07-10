544d295d7b03bc9e2e60cf1ed4ff003455c28ef2
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-07-10 22:05:19 +1200

    Blog Update

--- includes/hero.js
+++ includes/hero.js
@@ -1,10 +1,17 @@
 document.write('<!-- Hero Image -->');
-document.write('<section id="hero-image">');
+document.write('<section id="hero-image" class="' + title + '">');
   document.write('<div id="hero-text">');
-    document.write('<img src="images/PilcrowMicro-Logo-Only-White.png" alt="PilcrowMicro">')
-    document.write('<p>My name is Anthony<p>');
-    document.write('<p>A Web Developer looking for 127.0.0.1</p>');
-    document.write('<p>I am PilcrowMicro</p>');
+    document.write('<img src="' + path + 'images/PilcrowMicro-Logo-Only-White.png" alt="PilcrowMicro">')
+    if (title == "Home"){
+      document.write('<p>My name is Anthony<p>');
+      document.write('<p>A Web Developer looking for 127.0.0.1</p>');
+      document.write('<p>I am PilcrowMicro</p>');
+    } else if (title == 'Blog'){
+      document.write('<p>My name is Anthony<p>');
+      document.write('<p>This is my blog.</p>');
+      document.write('<p>Spend a while perusing my musings.</p>');
+    }
+    
   document.write('</div>');
 document.write('</section>');
 
